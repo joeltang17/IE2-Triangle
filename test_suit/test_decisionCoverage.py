8381eca25461787354dbe54846e1556824d7d47c
joeltang17 <joeltang17@gmail.com>
2025-03-25 01:02:51 -0400

    Added comments

--- test_suit/test_decisionCoverage.py
+++ test_suit/test_decisionCoverage.py
@@ -2,42 +2,42 @@ import unittest
 from isTriangle import Triangle
 
 class TriangleTest(unittest.TestCase):
-    def test1(self):
+    def test1(self): #reaches triangle type equilateral
         actual = Triangle.classify(10, 10, 10)
         expected = Triangle.Type.EQUILATERAL
         self.assertEqual(actual, expected)
 
-    def testInvalidSide(self):
+    def testInvalidSide(self): #reaches first invalid statement from side being <= 0
         actual = Triangle.classify(0, 10, 10)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-    def testInvalidSide2(self):
+    def testInvalidSide2(self): #reaches second invalid statement from sum of two sides being less than the third
         actual = Triangle.classify(1, 2, 4)
         expected = Triangle.Type.INVALID    
         self.assertEqual(actual, expected)
 
-    def testScalene(self):
+    def testScalene(self): #reaches triangle type scalene
         actual = Triangle.classify(3, 4, 5)
         expected = Triangle.Type.SCALENE
         self.assertEqual(actual, expected)
 
-    def testIsosceles(self):
+    def testIsosceles(self): #reaches first isoceles statement (a == b )
         actual = Triangle.classify(3, 3, 4)
         expected = Triangle.Type.ISOSCELES  
         self.assertEqual(actual, expected)
 
-    def testIsosceles2(self):
+    def testIsosceles2(self): #reaches second isoceles statement (b == c)
         actual = Triangle.classify(3, 4, 3)
         expected = Triangle.Type.ISOSCELES
         self.assertEqual(actual, expected)
 
-    def testIsosceles3(self):
+    def testIsosceles3(self): #reaches third isoceles statement (a == c)
         actual = Triangle.classify(4, 3, 3)
         expected = Triangle.Type.ISOSCELES
         self.assertEqual(actual, expected)
 
-    def testInvalidLastSide(self):
+    def testInvalidLastSide(self): #reaches last invalid statement from sum of two equal sides being less than the third
         actual = Triangle.classify(3, 3, 10)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
