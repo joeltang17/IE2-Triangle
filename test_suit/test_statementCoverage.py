9b996e99e0c8344604b4dcc242ef70cdeed44ed2
joeltang17 <joeltang17@gmail.com>
2025-03-25 00:12:49 -0400

    Finished statement coverage

--- test_suit/test_statementCoverage.py
+++ test_suit/test_statementCoverage.py
@@ -7,5 +7,35 @@ class TriangleTest(unittest.TestCase):
         expected = Triangle.Type.EQUILATERAL
         self.assertEqual(actual, expected)
 
+    def testInvalidSide(self):
+        actual = Triangle.classify(0, 10, 10)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+
+    def testInvalidSide2(self):
+        actual = Triangle.classify(1, 2, 4)
+        expected = Triangle.Type.INVALID    
+        self.assertEqual(actual, expected)
+
+    def testScalene(self):
+        actual = Triangle.classify(3, 4, 5)
+        expected = Triangle.Type.SCALENE
+        self.assertEqual(actual, expected)
+
+    def testIsosceles(self):
+        actual = Triangle.classify(3, 3, 4)
+        expected = Triangle.Type.ISOSCELES  
+        self.assertEqual(actual, expected)
+
+    def testIsosceles2(self):
+        actual = Triangle.classify(3, 4, 3)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+
+    def testIsosceles3(self):
+        actual = Triangle.classify(4, 3, 3)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+
 if __name__ == '__main__':
     unittest.main()
